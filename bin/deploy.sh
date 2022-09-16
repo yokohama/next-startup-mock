b8e2b0d048aae6e0a5d936efeb02b20e7fb6ead1
yokohama <yuhei.yokohama@gmail.com>
2022-09-17 01:12:56 +0900

    init

---
+++ bin/deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+ENV_PREFIX='NextStartUpApi-'
+
+if [ "$TARGET_ENV" == "dev" ]; then
+  TARGET_ENV=dev
+elif [ "$TARGET_ENV" == "prod" ]; then
+  TARGET_ENV=prod
+else
+  TARGET_ENV=local
+fi
+YAML_FILE="./openapi/root-${TARGET_ENV}.yaml"
+
+ITEMS=`aws apigateway get-rest-apis`
+
+for item in $(echo $ITEMS | jq -c '.items[]'); do
+  TARGET_NAME=`echo $item | jq -r '.name'`
+  if [ "${ENV_PREFIX}${TARGET_ENV}" == "$TARGET_NAME" ]; then
+    API_GATEWAY_ID=`echo $item | jq -r '.id'`
+  fi
+done
+
+aws apigateway put-rest-api \
+  --mode overwrite \
+  --cli-binary-format raw-in-base64-out \
+  --rest-api-id ${API_GATEWAY_ID} \
+  --body "file://${YAML_FILE}"
+
+exit 0
